efed02567a20a3e9e720499a90f5f2b490553d6d
Diego <djvarela89@gmail.com>
2023-01-21 20:35:30 -0300

    corrigiendo script

--- notificacion.php
+++ notificacion.php
@@ -9,30 +9,24 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 
 $mailer = new PHPMailer();
-
-
 $mailer->CharSet="UTF-8";
 
 $hoy = date("d-m");
 
-
-
-
-$cnx = $connection;
 $c = "SELECT * FROM cumples WHERE FECHA = '$hoy'";
 $result = $cnx->prepare($c);
 $result->execute();
 
 while($row = $result->fetch()){
     $nombre = $row['NOMBRE'];
-    $cumple = $row['FECHA'];
+    $fecha = $row['FECHA'];
     $destinatario = $row['EMAIL'];
 
 };
 
 
-$emisor= "diego@mail.com";
-$mombreEmisor = "Diego";
+$emisor= "evento@almancron.com";
+$mombreEmisor = "ALMANCRON";
 $asunto = "Notificacion de recordatorio";
 
 
@@ -40,16 +34,15 @@ $asunto = "Notificacion de recordatorio";
 $mailer->setFrom( $emisor, "$mombreEmisor" ) ;
 $mailer->addAddress( $destinatario );
 $mailer->Subject= $asunto;
-$mailer->msgHTML("Mensaje de cumple:  $nombre ");
+$mailer->msgHTML("Motivo de evento:  $fecha ");
 
 
-if(  $cumple == $hoy ){
-    echo "¡es hoy!";
-    echo "cumple $nombre";
+if(  $fecha == $hoy ){
+    echo "Motivo de evento:  $nombre  |  $fecha";
     $mailer->send();
 }else{
 
-    echo "No hay evento para hoy";
+    echo "No hay eventos registrados para hoy";
 };
 
 ?>
